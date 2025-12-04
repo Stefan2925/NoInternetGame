aa8a6bd2d16fb90a60b5266558fe7bc7bd43576e
AndreiBcs <andreibichis1@gmail.com>
2025-12-04 20:55:19 +0200

    am realizat animatia de jump a PLAYER-ului

--- DinoGame/Form1.cs
+++ DinoGame/Form1.cs
@@ -17,13 +17,16 @@ namespace DinoGame
         int[][] Board;
         Graphics mDesen;
         SolidBrush[] pensula;
-        int dinoX;
-        int dinoY;
-        int dinoJump = 2;//asta ma gandeam sa o folosim pentru saritura,
-                         //atunci cand apesi space PLAYER-ul sare cu 2 pozitii in sus ca sa evitam obstacolele
+        int dinoX=12;
+        int dinoY=10;
+        int dinoJump = 3;//asta ma gandeam sa o folosim pentru saritura,
+                         //atunci cand apesi space PLAYER-ul sare cu 3 pozitii in sus ca sa evitam obstacolele
         bool isJumping = false;
+        bool isUp = false;
+        int jumpStep = 0;//numarul de pasi facuti in saritura
         int ground=10;//linia solului
         Random rand = new Random();//pentru obstacole
+        private Timer gameTimer;
 
         public enum PATRATE
         {
@@ -41,8 +44,19 @@ namespace DinoGame
             pensula[0] = new SolidBrush(Color.White);
             pensula[1] = new SolidBrush(Color.Blue);
             pensula[2] = new SolidBrush(Color.Red);
+
+            gameTimer = new Timer();
+            gameTimer.Interval = 100;
+            gameTimer.Tick += new EventHandler(GameTimer);
+            this.KeyPreview = true;
             this.KeyDown += pressSpace;
 
+        }
+
+        private void GameTimer(object sender, EventArgs e)
+        {
+            UpdateGame();
+            drawBoard();
         }
 
         private void Btn_Exit_Click(object sender, EventArgs e)
@@ -62,11 +76,22 @@ namespace DinoGame
                     Board[i][j] = (int)PATRATE.GOL;
                 }
             }
+            dinoY = ground;
+            gameTimer.Start();
+            this.ActiveControl = null;
             drawBoard();
+
         }
 
         void drawBoard()
         {
+            mDesen.Clear(Color.White);
+
+            for (int i = 0; i < 60; i++)
+                Array.Clear(Board[i], 0, 20);
+
+            drawDino(dinoY);
+
             for (int i = 0; i < 60; i++)
             {
                 for (int j = 0; j < 20; j++)
@@ -77,22 +102,52 @@ namespace DinoGame
 
         }
 
-        private void Game(object sender, EventArgs e)
+        void drawDino(int y)
         {
-            dinoX = 12;
-            dinoY = 10;
-            Board[dinoX][dinoY] = (int)PATRATE.PLAYER;
-            mDesen.FillRectangle(pensula[Board[dinoX][dinoY]], dinoX * DIMENSIUNE + 1, dinoY * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1);
-
-
-            drawBoard();
+            Board[dinoX][y] = (int)PATRATE.PLAYER;
+            mDesen.FillRectangle(pensula[(int)PATRATE.PLAYER], dinoX * DIMENSIUNE + 1, dinoY * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1);
         }
+
+        private void UpdateGame()
+        {
+            if (!isJumping)
+            {
+                return;
+            }
+            if (isUp)
+            {
+                if (dinoY > 0)
+                {
+                    dinoY--;
+                    jumpStep++;
+
+                }
+                if (jumpStep >= dinoJump)
+                {
+                    isUp = false;
+                }
+            }
+            else
+            {
+                if (dinoY < ground)
+                {
+                    dinoY++;
+                }
+                else
+                {
+                    isJumping = false;
+                    jumpStep = 0;
+                    isUp = false;
+                }
+            }
+        }       
         private void pressSpace(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Space && !isJumping)
             {
                 isJumping = true;
-                dinoJump = -2;
+                isUp = true;
+                jumpStep = 0;
             }
         }
 
