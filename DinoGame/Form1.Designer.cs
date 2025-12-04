630efcca3f8e4e6782aa82295ce77b98f32e007f
CretaciousStrategist <stefi.deac25@gmail.com>
2025-12-04 17:17:27 +0200

    Board

--- DinoGame/Form1.Designer.cs
+++ DinoGame/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace DinoGame
 {
-    partial class Form1
+    partial class FormDino
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Button Btn_Exit;
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            Btn_Exit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1201, 401);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Btn_Exit
+            // 
+            Btn_Exit.Location = new System.Drawing.Point(1219, 187);
+            Btn_Exit.Name = "Btn_Exit";
+            Btn_Exit.Size = new System.Drawing.Size(61, 47);
+            Btn_Exit.TabIndex = 1;
+            Btn_Exit.Text = "Btn_Exit";
+            Btn_Exit.UseVisualStyleBackColor = true;
+            Btn_Exit.Click += new System.EventHandler(this.Btn_Exit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1219, 283);
+            this.button1.Name = "Play";
+            this.button1.Size = new System.Drawing.Size(51, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // FormDino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1282, 473);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(Btn_Exit);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "FormDino";
+            this.Text = "FormDino";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- DinoGame/Form1.cs
+++ DinoGame/Form1.cs
@@ -10,11 +10,64 @@ using System.Windows.Forms;
 
 namespace DinoGame
 {
-    public partial class Form1 : Form
+    public partial class FormDino : Form
     {
-        public Form1()
+        int DIMENSIUNE=20;
+        PictureBox pictureBox;
+        int [][] Board;
+        Graphics mDesen;
+        SolidBrush [] pensula;
+
+       public enum PATRATE{
+            GOL=1,
+            PLAYER=2,
+            OBSTACOL =3,
+
+        }
+        public FormDino()
         {
             InitializeComponent();
+            mDesen = pictureBox1.CreateGraphics();
+          
+            pensula = new SolidBrush[3];
+            pensula[0] = new SolidBrush(Color.White);
+            pensula[1] = new SolidBrush(Color.Red);
+            pensula[2] = new SolidBrush(Color.Blue);
+
+
+
+
+
+
+        }
+       
+       
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+
+          
+            Board = new int[60][];
+            for (int i = 0; i < 60; i++)
+            {
+                Board[i] = new int[20];
+                for (int j = 0; j < 20; j++)
+                {
+                    Board[i][j] = (int)PATRATE.GOL;
+                }
+            }
+
+            for (int i = 0; i < 60; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    mDesen.FillRectangle(pensula[Board[i][j]], i * DIMENSIUNE + 1, j * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1);
+                }
+            }
         }
     }
 }
