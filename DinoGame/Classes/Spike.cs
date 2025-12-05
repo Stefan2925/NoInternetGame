00b86cd99a8a29a15fc20217604db5049bab1c7a
CretaciousStrategist <stefi.deac25@gmail.com>
2025-12-05 08:54:15 +0200

    am bagat spike-ul si noroi, for-urile de la Drawbox is cam aglomerate daca stii cumva sa fie mai simplu modifica si daca ai citit ce  am comentat  da-le delete ca sa nu mai aglomeram cu mesaje

---
+++ DinoGame/Classes/Spike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoGame.Classes
+{
+    internal class Spike
+    {
+       
+        public Point[] spike;
+      public   Spike(int DIMENSIUNE)
+        {
+            spike = new Point[3];
+        
+            spike[0] = new Point(59*DIMENSIUNE + DIMENSIUNE / 2, 10*DIMENSIUNE);        
+            spike[1] = new Point(59*DIMENSIUNE, 10*DIMENSIUNE + DIMENSIUNE); 
+            spike[2] = new Point(59*DIMENSIUNE + DIMENSIUNE, 10*DIMENSIUNE + DIMENSIUNE); //Logica la chestia asta e sub clasa asta
+        }
+
+    }
+}
+//practic fillPolygon va lua patratul nostru de 20 pe 20 si il va transforma intr-o matrice de 400 de pixeli
+//apoi 3 puncte vor fi pozitionate pe aceasta matrice si la final se vor unii,tot ceea ce este  in interiorul acestei forme geometrice 
+//determinate de unirea celor trei puncte va fi umplut cu culoarea aleasa
+// astfel la inceput toate punctele incep din stanga sus a matricii
+//deci la spike[0] 59*Dimensiune e locatia patratului a matricii de 400 pixeli pe matricea  noastra de 60x20*400(picture box)
+// iar +DIMENSIUNE/2 inseamna ca muta primul punct la mijlocul matricii in partea de sus ,deoarece Y nu adauga cu nimic
+//la spike[1]  muta al doilea punct in coltul din stanga jos ,lasa x neschimbat 59 *DIMENSIUNE,dar la y pe langa  10*DIM ii mai baga 
+//inca 20 pixeli ca sa fie in stanga jos
+//la spike [2] la fel ca la spie[1] doar ca in dreapta jos a matricii
+//la final le uneste si coloreaza si ai spike-ul;
--- DinoGame/Form1.cs
+++ DinoGame/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DinoGame.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -28,12 +29,20 @@ namespace DinoGame
         int dinoY=10;
         int dinoJump = 3;//asta ma gandeam sa o folosim pentru saritura,
                          //atunci cand apesi space PLAYER-ul sare cu 3 pozitii in sus ca sa evitam obstacolele
+        int laCateSecunde = int.MaxValue;  //doar am initializat randomizarea obstacolului
+        int TipObstacol;// Sa fie mai multe tipuri de obstacole nu doar unul singur 
         bool isJumping = false;
         bool isUp = false;
         int jumpStep = 0;//numarul de pasi facuti in saritura
         int ground=10;//linia solului
         Random rand = new Random();//pentru obstacole
         private Timer gameTimer;
+
+
+        public Point[] spike = new Point[3]; //point reprezinta o structura de forma  int  x,int y (adica e facuta de csharp si reprezinta coordonata)
+       
+
+
 
         public enum PATRATE
         {
@@ -71,66 +80,24 @@ namespace DinoGame
         private void GameTimer(object sender, EventArgs e)
         {
             UpdateGame();
+           
             drawBoard();
         }
 
-        private void Btn_Exit_Click(object sender, EventArgs e)
+       void  GenerareObstacol()
         {
-            Application.Exit();
+           // if(tipObstacol ==1)
+            Board[59][10] = (int)PATRATE.OBSTACOL;
+            
         }
-
-        private void Play_Click(object sender, EventArgs e)
+        private void pressSpace(object sender, KeyEventArgs e)
         {
-
-            Board = new int[60][];
-            for (int i = 0; i < 60; i++)
+            if (e.KeyCode == Keys.Space && !isJumping)
             {
-                Board[i] = new int[20];
-                for (int j = 0; j < 20; j++)
-                {
-                    Board[i][j] = (int)PATRATE.GOL;
-                }
+                isJumping = true;
+                isUp = true;
+                jumpStep = 0;
             }
-            dinoY = ground;
-            gameTimer.Start();
-            this.ActiveControl = null;
-            drawBoard();
-
-        }
-
-        void drawBoard()
-        {
-            g.Clear(Color.White);
-
-            for (int i = 0; i < 60; i++)
-                Array.Clear(Board[i], 0, 20);
-
-            drawDino(dinoY);
-
-            for (int i = 0; i < 60; i++)
-            {
-                for (int j = 0; j < 20; j++)
-                {
-                    Brush b;
-
-                    if (j < 11)
-                        b = pensula[Board[i][j]];
-                    else if (j == 11)
-                        b = pensula[(int)PATRATE.IARBA];
-                    else
-                        b = pensula[(int)PATRATE.NOROI];
-
-                    g.FillRectangle(b,  i * DIMENSIUNE + 1,  j * DIMENSIUNE + 1, DIMENSIUNE - 1,DIMENSIUNE - 1);
-                }
-            }
-
-            pictureBox1.Refresh(); 
-        }
-
-
-        void drawDino(int y)
-        {                                                                 //fill Rectangle  nu era necesar
-            Board[dinoX][y] = (int)PATRATE.PLAYER; //adineauri desenai dino-ul aici dar nu era nevoie ca oricum in drawboard il deseneaza inca odata
         }
 
         private void UpdateGame()
@@ -165,15 +132,82 @@ namespace DinoGame
                     isUp = false;
                 }
             }
-        }       
-        private void pressSpace(object sender, KeyEventArgs e)
+        }
+       
+
+        void drawBoard()
         {
-            if (e.KeyCode == Keys.Space && !isJumping)
+            g.Clear(Color.White); //face by default totul alb fara sa se vada delimitarile patratelelor 
+                                  //ma gandeam ca am putea folosi un dinasta pentru iarba sau noroi sa fie si pamantul cu textura smooth fara patrate
+
+            for (int i = 0; i < 60; i++)
+                Array.Clear(Board[i], 0, 20);   //fara chestia asta nu se mentine background-ul si player-ul da overrun
+                                                 //daca vrei sa vezi da-i delete la tot for-ul si apoi dai start si sari odata;
+            drawDino(dinoY);
+            GenerareObstacol();
+
+            for (int i = 0; i < 60; i++)
             {
-                isJumping = true;
-                isUp = true;
-                jumpStep = 0;
+                for (int j = 0; j < 20; j++)
+                {
+                    Brush b; //mostenire
+
+                    if (j < 11)
+                        b = pensula[Board[i][j]];
+                    else if (j == 11)
+                        b = pensula[(int)PATRATE.IARBA];
+                    else
+                        b = pensula[(int)PATRATE.NOROI];
+                    if (Board[i][j] == (int)PATRATE.OBSTACOL)
+                    {
+
+
+                      Classes.Spike temp = new Classes.Spike(DIMENSIUNE);
+
+                        g.FillPolygon(b, temp.spike);
+                    }
+                    else { g.FillRectangle(b, i * DIMENSIUNE + 1, j * DIMENSIUNE + 1, DIMENSIUNE - 1, DIMENSIUNE - 1); }
+                    
+                }
             }
+
+            pictureBox1.Refresh(); 
+        }
+
+        void MoveObstacol() //nu are rost ca toata tabla sa se miste spre stanga,obstacolele vor veni din dreapta  cu timer-ul asta si tu trebie doar sa sari peste 
+        {                   
+
+        }
+
+
+        void drawDino(int y)
+        {                                                                 //fill Rectangle  nu era necesar
+            Board[dinoX][y] = (int)PATRATE.PLAYER; //adineauri desenai dino-ul aici dar nu era nevoie ca oricum in drawboard il deseneaza inca odata
+        }
+
+      
+
+        private void Btn_Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Play_Click(object sender, EventArgs e)
+        {
+
+            Board = new int[60][];
+            for (int i = 0; i < 60; i++)
+            {
+                Board[i] = new int[20];
+                for (int j = 0; j < 20; j++)
+                {
+                    Board[i][j] = (int)PATRATE.GOL;
+                }
+            }
+            dinoY = ground;
+            gameTimer.Start();
+            this.ActiveControl = null;
+            drawBoard();
+
         }
 
 
